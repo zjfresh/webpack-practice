9b6ccdd4c0d79959f8a808ed8867fe72bf959018
zj21 <zj21@meitu.com>
2021-01-18 17:27:12 +0800

    :sparkles: extract common chunk among entries

---
+++ dist/common.bundle.js
@@ -0,0 +1,12 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[0],[
+/* 0 */
+/***/ (function(module, exports) {
+
+module.exports = function(...args) {
+    return args.reduce((prev, next) => {
+        return prev + next
+    })
+}
+
+/***/ })
+]]);
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const fs = require('fs');
 
 const webpackChain = require('webpack-chain');
 
@@ -6,6 +7,24 @@ const ESLintPlugin = require('eslint-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 let config = new webpackChain();
+
+
+// (function deleteall(path) {
+//     var files = [];
+//     if(fs.existsSync(path)) {
+//         files = fs.readdirSync(path);
+//         files.forEach(function(file, index) {
+//             var curPath = path + "/" + file;
+//             if(fs.statSync(curPath).isDirectory()) { // recurse
+//                 deleteall(curPath);
+//             } else { // delete file
+//                 fs.unlinkSync(curPath);
+//             }
+//         });
+//         fs.rmdirSync(path);
+//     }
+// }('dist'))
+
 
 // config.mode('development')
 config.mode('production')
@@ -19,6 +38,35 @@ config.mode('production')
     
     .optimization
     .minimize(false)
+    .splitChunks({
+        // chunks: 'initial',
+        // // minSize: 30000,
+        // minSize: 1,
+        // maxSize: 0,
+        // minChunks: 1,
+        // maxAsyncRequests: 5,
+        // maxInitialRequests: 3,
+        // automaticNameDelimiter: '~',
+        // automaticNameMaxLength: 30,
+        // name: true,
+        // cacheGroups: {
+        //   default: {
+        //     minChunks: 2,
+        //     priority: -20,
+        //     reuseExistingChunk: true
+        //   }
+        // }
+        cacheGroups: {
+            default: {
+                name: 'common',
+                // priority: -20,
+                minSize: 1,
+                minChunks: 2, // 限定被拆分的 Chunk 至少被多少个模块引用，不设置或设置为1将导致所有模块被抽离进来
+                // reuseExistingChunk: true,
+                chunks: 'initial'
+            }
+        }
+      })
     .end()
 
     .output /* .path(path.resolve('dist')) */
